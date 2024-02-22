47805d7819c77d855defbebb40460cd5fb43ad26
Pedro <vidadeenderman@gmail.com>
2024-02-22 16:54:16 -0300

    comitar 1, pycharm

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import plotly
+
+# ndarray1 = np.zeros(100000)
+# print(f' 1 - Conteúdo da lista: {ndarray1}, o tamanho: {len(ndarray1)}')
+#
+# ndarray2 = np.ones(100000)
+# print(f' 1 - Conteúdo da lista: {ndarray2}, o tamanho: {len(ndarray2)}')
+#
+ndarray3 = np.linspace(10, 1000, 1000)
+print(f' 1 - Conteúdo da lista: {ndarray3}, o tamanho: {len(ndarray3)}')
